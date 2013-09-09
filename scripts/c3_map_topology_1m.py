32ad06930936f855086f20ad4d3e33de2150b739
zehpunktbarron <christopher.barron@gmx.de>
2013-09-09 16:32:56 +0200

    Update c3_map_topology_1m.py

--- scripts/c3_map_topology_1m.py
+++ scripts/c3_map_topology_1m.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python2.7
 
 #description     :This file creates a map: Calculates all roads which don't share a common start- or endpoint an lie within 1m
-#author          :Christopher Barron
+#author          :Christopher Barron @ http://giscience.uni-hd.de/
 #date            :19.01.2013
 #version         :0.1
 #usage           :python pyscript.py
