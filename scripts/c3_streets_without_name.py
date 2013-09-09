10c366185a84c6793dfd582b3e426fb79081fdb0
zehpunktbarron <christopher.barron@gmx.de>
2013-09-09 16:34:06 +0200

    Update c3_streets_without_name.py

--- scripts/c3_streets_without_name.py
+++ scripts/c3_streets_without_name.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python2.7
 
 #description     :This file creates a plot: Calculates the length [km] of all streets without a name. Results are grouped by street category
-#author          :Christopher Barron
+#author          :Christopher Barron @ http://giscience.uni-hd.de/
 #date            :19.01.2013
 #version         :0.1
 #usage           :python pyscript.py
