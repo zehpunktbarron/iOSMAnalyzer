1745c180759d5641dd548a847e7228f127cfee9b
zehpunktbarron <christopher.barron@gmx.de>
2013-09-09 17:46:15 +0200

    Update c6_invalid_poly.py

--- scripts/c6_invalid_poly.py
+++ scripts/c6_invalid_poly.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python2.7
 
 #description     :This file creates a plot: Calculates the development of invalid polygons (total amount and percentage)
-#author          :Christopher Barron
+#author          :Christopher Barron  @ http://giscience.uni-hd.de/
 #date            :19.01.2013
 #version         :0.1
 #usage           :python pyscript.py
