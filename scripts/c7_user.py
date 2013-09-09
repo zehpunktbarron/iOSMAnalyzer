cd1e53d9d5cc888a1668c96134f9711706515ff4
zehpunktbarron <christopher.barron@gmx.de>
2013-09-09 17:51:14 +0200

    Update c7_user.py

--- scripts/c7_user.py
+++ scripts/c7_user.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python2.7
 
 #description     :This file creates a plot: Development of contributers with their first contribution per month
-#author          :Christopher Barron
+#author          :Christopher Barron  @ http://giscience.uni-hd.de/
 #date            :19.01.2013
 #version         :0.1
 #usage           :python pyscript.py
