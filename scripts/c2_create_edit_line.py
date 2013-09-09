38796a1bda906fbb153516d766ca6a81de29f877
zehpunktbarron <christopher.barron@gmx.de>
2013-09-09 12:40:17 +0200

    Update c2_create_edit_line.py

--- scripts/c2_create_edit_line.py
+++ scripts/c2_create_edit_line.py
@@ -3,7 +3,7 @@
 
 #title           :All created and edited line
 #description     :This file creates a plot: How many lines have been created or edited per month?
-#author          :Christopher Barron
+#author          :Christopher Barron @ http://giscience.uni-hd.de/
 #date            :19.01.2013
 #version         :0.1
 #usage           :python pyscript.py
