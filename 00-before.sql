446c8807831994ae9ba1c67f105c49ac3af411d1
zehpunktbarron <christopher.barron@gmx.de>
2014-03-01 21:50:58 +0100

    Create 00-before.sql
    File for the osm-history-renderer. Add the lines marked with "aaaaaaaa".

---
+++ 00-before.sql
@@ -0,0 +1,115 @@
+-- requires hstore_new, postgis, gist_btree
+
+DROP TABLE IF EXISTS hist_point CASCADE;
+CREATE TABLE hist_point (
+    -- optional 64bit support
+    id BIGINT,
+    -- id int,
+    version smallint,
+    visible boolean,
+    valid_from timestamp without time zone,
+    valid_to timestamp without time zone,
+    user_id BIGINT NOT NULL,			--aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    user_name text,				--aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    tags hstore
+);
+SELECT AddGeometryColumn(
+    -- table name
+    'hist_point',
+
+    -- column name
+    'geom',
+
+    -- SRID (900913 = Spherical Mercator)
+    900913,
+
+    -- type
+    'POINT',
+
+    -- dimensions
+    2
+);
+
+
+DROP TABLE IF EXISTS hist_line CASCADE;
+CREATE TABLE hist_line (
+    -- optional 64bit support
+    id BIGINT,
+    -- id int,
+    version smallint,
+    minor smallint,
+    visible boolean,
+    valid_from timestamp without time zone,
+    valid_to timestamp without time zone,
+    user_id BIGINT NOT NULL,			--aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    user_name text,				--aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    tags hstore,
+    z_order integer
+);
+SELECT AddGeometryColumn(
+    -- table name
+    'hist_line',
+
+    -- column name
+    'geom',
+
+    -- SRID (900913 = Spherical Mercator)
+    900913,
+
+    -- type
+    'LINESTRING',
+
+    -- dimensions
+    2
+);
+
+
+DROP TABLE IF EXISTS hist_polygon CASCADE;
+CREATE TABLE hist_polygon (
+    -- optional 64bit support
+    id BIGINT,
+    -- id int,
+    version smallint,
+    minor smallint,
+    visible boolean,
+    valid_from timestamp without time zone,
+    valid_to timestamp without time zone,
+    user_id BIGINT NOT NULL,			--aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    user_name text,				--aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    tags hstore,
+    z_order integer,
+    area real
+);
+SELECT AddGeometryColumn(
+    -- table name
+    'hist_polygon',
+
+    -- column name
+    'geom',
+
+    -- SRID (900913 = Spherical Mercator)
+    900913,
+
+    -- type -- needs to be changed to generic GEOMETRY later (maybe)
+    --'MULTIPOLYGON',
+    'POLYGON',
+
+    -- dimensions
+    2
+);
+SELECT AddGeometryColumn(
+    -- table name
+    'hist_polygon',
+
+    -- column name
+    'center',
+
+    -- SRID (900913 = Spherical Mercator)
+    900913,
+
+    -- type
+    'POINT',
+
+    -- dimensions
+    2
+);
